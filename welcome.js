ae65686446cf7f2158f8abd6e07986925152f594
JayeshYadav99 <107855172+JayeshYadav99@users.noreply.github.com>
2022-09-19 11:01:38 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,10 +1,10 @@
-firebase.auth().onAuthStateChanged((user) => {
-  if (!user) {
-    location.replace("index.html");
-  } else {
-    document.getElementById("user").innerHTML = "Hello, " + user.email;
-  }
-});
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (!user) {
+//     location.replace("index.html");
+//   } else {
+//     document.getElementById("user").innerHTML = "Hello, " + user.email;
+//   }
+// });
 
 function logout() {
   firebase.auth().signOut();
