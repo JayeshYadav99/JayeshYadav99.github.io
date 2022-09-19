6afab298535a2920d0f04dc35c19920862003603
JayeshYadav99 <107855172+JayeshYadav99@users.noreply.github.com>
2022-09-19 14:03:39 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,17 @@
-function logout() {
-    firebase.auth().signOut();
-  }
-  
-  window.onbeforeunload = function () {
-    firebase.auth().signOut();
-  };
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
 
   
