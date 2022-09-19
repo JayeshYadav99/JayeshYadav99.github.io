9380306ca6b1c3b7d429c7d8f8ad49f530f8386f
JayeshYadav99 <107855172+JayeshYadav99@users.noreply.github.com>
2022-09-19 16:44:44 +0530

    Update generatde.js

--- generatde.js
+++ generatde.js
@@ -10,45 +10,48 @@ btn.addEventListener("click", Value);
 getZoneValue();
 function getZoneValue() {
   zone = document.getElementById("ZONE").value;
-  localStorage.setItem("Zone", zone);
+  
 }
 
 getRangeValue();
 function getRangeValue() {
   range = document.getElementById("RANGE").value;
-  localStorage.setItem("Range", range);
+  
 }
 getDistrictValue();
 function getDistrictValue() {
   district = document.getElementById("DISTRICT").value;
-  localStorage.setItem("district", district);
+  
 }
 getSubdivison();
 function getSubdivison() {
   subdivision = document.getElementById("SUBDIVISION").value;
-  localStorage.setItem("subdivison", subdivision);
+  
 }
 getPolicestation();
 function getPolicestation() {
   policestation = document.getElementById("POLICESTATION").value;
-  localStorage.setItem("policestation", policestation);
+  
 }
 
-function Value() {
-  if (district == "vadodara") {
-    if (policestation == "Sayajiganj") {
-      window.location.href = "Code1.png";
-    }
-    if (policestation =="Chhani") {
-      window.location.href ="Code2.png";
-    }
+function Value()
+ {
+  if (district == "vadodara"&&policestation=="Sayajiganj") 
+  {
+  window.location.href="Code1.png";
   }
-  if (district == "Bharuch") {
-    if (policestation == "Sindhwai") {
-      window.location.href = "Code3.png";
-    }
-    if (policestation == "MotaBazar") {
-      window.location.href= "Code4.png";
-    }
+  if(district == "vadodara"&&policestation=="Chhani")
+  {
+    window.location.href="Code2.png";
+
   }
+  if(district == "bharuch"&&policestation=="Sindhwai")
+  {
+    window.location.href="Code3.png";
+  }
+  if(district == "bharuch"&&policestation=="MotaBazar")
+  {
+    window.location.href="Code4.png";
+  }
+
 }
