4c1b133c284248f3425edd938e52bc6b5bebdee9
JayeshYadav99 <107855172+JayeshYadav99@users.noreply.github.com>
2022-09-19 22:14:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,17 +36,17 @@ function forgotPass(){
         document.getElementById("error").innerHTML = error.message
     });
 }
-firebase.auth().setPersistence('SESSION')
-  .then(() => {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
-    return firebase.auth().signInWithEmailAndPassword(email, password);
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(() => {
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      return firebase.auth().signInWithEmailAndPassword(email, password);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
