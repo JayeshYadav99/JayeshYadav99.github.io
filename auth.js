d0ebca7843ac685f59dfda299c84cff328b9cbe6
JayeshYadav99 <107855172+JayeshYadav99@users.noreply.github.com>
2022-09-20 09:55:27 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,13 +1,15 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyDqOmdqmsGNhDOrjy_xEIjZ-JnKq7gMBlE",
-    authDomain: "my-noob-7739a.firebaseapp.com",
-    databaseURL: "https://my-noob-7739a-default-rtdb.firebaseio.com",
-    projectId: "my-noob-7739a",
-    storageBucket: "my-noob-7739a.appspot.com",
-    messagingSenderId: "829264938526",
-    appId: "1:829264938526:web:73dcf661bdb3d0a7cbd00f",
-    measurementId: "G-VDTL26G5DT"
+  apiKey: "AIzaSyDqOmdqmsGNhDOrjy_xEIjZ-JnKq7gMBlE",
+  authDomain: "my-noob-7739a.firebaseapp.com",
+  databaseURL: "https://my-noob-7739a-default-rtdb.firebaseio.com",
+  projectId: "my-noob-7739a",
+  storageBucket: "my-noob-7739a.appspot.com",
+  messagingSenderId: "829264938526",
+  appId: "1:829264938526:web:86fa140189c90fcfcbd00f",
+  measurementId: "G-SLHR6BV766"
 };
+
+
 	firebase.initializeApp(firebaseConfig);
 render();
 function render(){
