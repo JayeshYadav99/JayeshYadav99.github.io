dfeb4f558d8ce4826cbc4c008e1bd32e2ce9fe52
JayeshYadav99 <jayeshyadav528@gmail.com>
2022-09-21 09:40:02 +0530

    c

--- gengeneratde_1.js
+++ gengeneratde_1.js
@@ -1,15 +1,3 @@
-firebase.auth().onAuthStateChanged((user) => {
-  if (!user) {
-    location.replace("index.html");
-  } else {
-    document.getElementById("user").innerHTML = "Hello, " + user.email;
-  }
-});
-
-function logout() {
-
-  firebase.auth().signOut();
-}
 var zone;
 var range;
 var district;
